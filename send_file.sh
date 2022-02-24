e5fbfd75943cc56709643ef8955c641e77422834
sachigang <93555425+sachigang@users.noreply.github.com>
2022-02-24 16:49:56 +0530

    Updating send_file.sh

--- send_file.sh
+++ send_file.sh
@@ -9,18 +9,18 @@ PASSWORD="mypass"
 # $1 is the first argument to the script
 # We are using it as upload directory path
 # If it is '.', file is uploaded to current directory.
-DESTINATION=$1
+#DESTINATION=$1
 
 
 # Rest of the arguments are a list of files to be uploaded.
 # ${@:2} is an array of arguments without first one.
-ALL_FILES="${@:2}"
+#ALL_FILES="${@:2}"
 
 
 # FTP login and upload is explained in paragraph below
 ftp -inv $HOST <<EOF
 user $USER $PASSWORD
-cd $DESTINATION
-mput $ALL_FILES
+#cd $DESTINATION
+put greeting
 bye
 EOF
